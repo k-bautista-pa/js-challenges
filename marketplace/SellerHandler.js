a556cbb651d78bbea0263a0a379032244e34b2e1
k-bautista-pa <kristineb@providerassist.com.au>
2020-03-04 18:28:41 +0800

    implement update and delete seller

---
+++ marketplace/SellerHandler.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const httpStatusCode = require('http-status-codes');
+const { OK, CREATED, NOT_FOUND } = httpStatusCode;
+
+const db = require('./db_connect');
+const constants = require('./util/constants');
+const { TABLES } = constants;
+const { SELLER_TABLE } = TABLES;
+const helpers = require('./util/helpers');
+const { successMessage, errorMessage } = helpers;
+
+
+module.exports.getAllSellers = async event => {
+  try {
+    const rows = await db.getAll(SELLER_TABLE);
+    return successMessage(OK, {sellers: rows});
+  }
+  catch(error) {
+    console.log('[SELLER] GET All error: ', error);
+    return errorMessage(error);
+  }
+}
+
+module.exports.createSeller = async event => {
+  try {
+    const data = JSON.parse(event.body);
+    await db.insert(SELLER_TABLE, data);
+    return successMessage(CREATED, {message: `Succesfully created user ${data.name}.`});
+  }
+  catch(error) {
+    console.log('[SELLER] Create seller error: ', error);
+    return errorMessage(error);
+  }
+}
+
+module.exports.updateSeller = async event => {
+  try {
+    const id = event.pathParameters.id;
+    const data = JSON.parse(event.body);
+    const record = await db.getById(SELLER_TABLE, id);
+    
+    if(record) {
+      await db.updateById(SELLER_TABLE, id, data);
+      return successMessage(OK, {message: `Successfully updated user ${record.name}.`});
+    }
+
+    return errorMessage({statusCode: NOT_FOUND, message: 'User does not exist.'});
+  }
+  catch(error) {
+    console.log('[SELLER] Update seller error: ', error);
+    return errorMessage(error);
+  }
+}
+
+module.exports.deleteSeller = async event => {
+  try {
+    const id = event.pathParameters.id;
+    const data = JSON.parse(event.body);
+    const record = await db.getById(SELLER_TABLE, id);
+    const username = record.name;
+    // @TODO: Soft or hard delete?
+    
+    if(record) {
+      await db.deleteById(SELLER_TABLE, id, data);
+      return successMessage(OK, {message: `Successfully deleted ${username}.`});
+    }
+
+    return errorMessage({statusCode: NOT_FOUND, message: 'User does not exist.'});
+  }
+  catch(error) {
+    console.log('[SELLER] Delete seller error: ', error);
+    return errorMessage(error);
+  }
+}
--- marketplace/util/helpers.js
+++ marketplace/util/helpers.js
@@ -1,7 +1,8 @@
 module.exports.errorMessage = error => {
+  console.log('Error:', error);
   return {
     statusCode: error.statusCode || 500,
-    body: `Error: Could not find sellers: ${error}`
+    body: JSON.stringify({error: error.message})
   }
 }
 
